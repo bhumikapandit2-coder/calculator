4cd460de2efd6952f54b49987f893db0d005ace7
bhumikapandit2-coder <bhumikapandit2@gmail.com>
2026-07-10 21:42:17 +0530

    Add files via upload

--- view_history.py
+++ view_history.py
@@ -1,1 +1,13 @@
+import sqlite3
 
+conn = sqlite3.connect("calculator.db")
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM history")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
