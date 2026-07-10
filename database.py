79c414809ad417fc1a3935b74b05b7c5b51aa92f
bhumikapandit2-coder <bhumikapandit2@gmail.com>
2026-07-10 21:41:18 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
+import sqlite3
 
+# Database Connection
+conn = sqlite3.connect("calculator.db")
+cursor = conn.cursor()
+
+# Table Create
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    num1 REAL,
+    operator TEXT,
+    num2 REAL,
+    result TEXT
+)
+""")
+
+conn.commit()
